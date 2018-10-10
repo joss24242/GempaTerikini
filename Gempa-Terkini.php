c2dbc63044c790890a3cfc2f5abb9eb0f4362ba6
joss24242 <39421166+joss24242@users.noreply.github.com>
2018-10-11 04:40:04 +0700

    Update Gempa-Terkini.php

--- Gempa-Terkini.php
+++ Gempa-Terkini.php
@@ -11,19 +11,19 @@ function main(){
 	print " Tanggal   : ".$data->gempa->Tanggal."\n";
 	print " Jam       : ".$data->gempa->Jam."\n";
 	print "\n";
-	print "\[\033[1;94m\]"; // BIBlue
+	print "\033[1;92m"; // BIBlue
 	print " Lintang   : ".$data->gempa->Lintang."\n";
 	print " Bujur     : ".$data->gempa->Bujur."\n";
 	print " Magnitude : ".$data->gempa->Magnitude."\n";
 	print " Kedalaman : ".$data->gempa->Kedalaman."\n";
 	print "\n";
-	print "\[\033[1;95m\]"; // BIPurple
+	print "\033[1;95m"; // BIPurple
 	print " Wilayah   : ".$data->gempa->Wilayah1."\n";
 	print " Potensi   : ".$data->gempa->Potensi."\n";
 	print "\033[0m"; // Normal
 }
 system('clear');
-print "\[\033[1;33m\]"; // BYellow
+print "\033[1;33m"; // BYellow
 print "============================================= \n";
 print "        INFO GEMPA DAN TSUNAMI TERINI         \n";
 print "  Gampa Terkini yang diambil dari data BMKG   \n";
