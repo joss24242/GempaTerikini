c15c3832381dc0bdefe10c63e2e9da3466c682ff
joss24242 <39421166+joss24242@users.noreply.github.com>
2018-10-11 04:29:47 +0700

    Update Gempa-Terkini.php

--- Gempa-Terkini.php
+++ Gempa-Terkini.php
@@ -11,28 +11,30 @@ function main(){
 	print " Tanggal   : ".$data->gempa->Tanggal."\n";
 	print " Jam       : ".$data->gempa->Jam."\n";
 	print "\n";
-	print "\033[0;36m"; // Cyan
+	print "\[\033[1;94m\]"; // BIBlue
 	print " Lintang   : ".$data->gempa->Lintang."\n";
 	print " Bujur     : ".$data->gempa->Bujur."\n";
 	print " Magnitude : ".$data->gempa->Magnitude."\n";
 	print " Kedalaman : ".$data->gempa->Kedalaman."\n";
 	print "\n";
-	print "\033[0;32m"; // Hijau
+	print "\[\033[1;95m\]"; // BIPurple
 	print " Wilayah   : ".$data->gempa->Wilayah1."\n";
 	print " Potensi   : ".$data->gempa->Potensi."\n";
 	print "\033[0m"; // Normal
 }
 system('clear');
-print "\033[01;31m"; // Merah tua
+print "\[\033[1;33m\]"; // BYellow
 print "============================================= \n";
 print "        INFO GEMPA DAN TSUNAMI TERINI         \n";
 print "  Gampa Terkini yang diambil dari data BMKG   \n";
+print "                                              \n";
 print "     Mod By     : Gebang Kiidiw               \n";
 print "     Contact Me : gebangkiidiw@gmail.com      \n";
 print "     Blog       : www.gebangkiidiw.com        \n";
 print "     Instagram  : @Bang_joss24                \n";
 print "     Thanks to  : Mr.Suhada                   \n";
-print " Semoga kita dalam lindungan ALLAH S.W.T      \n";
+print "                                              \n";
+print "  Semoga kita dalam lindungan ALLAH S.W.T     \n";
 print "============================================= \n";
 print "\033[0m"; // Normal
 main();
